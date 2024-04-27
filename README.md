8801915babd529153ac2ea89add73f2759344f69
Labhatorian <16213031+Labhatorian@users.noreply.github.com>
2024-04-27 13:38:56 +0200

    Create documentation for the second prototype

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
 # Leerroutes
-NPM packages voor visuele weergave van leerroutes HBO-ICT
+
+![GitHub Licentie](https://img.shields.io/github/license/Windesheim-HBO-ICT/Leerroutes)
+![Project Status Badge](https://img.shields.io/badge/status-in%20progress-brightgreen)
+![Versie Status Badge](https://img.shields.io/badge/versie-alpha-blue)
+
+NPM-packages voor het visueel weergeven van leerroutes binnen HBO-ICT.
+
+## Inhoudsopgave
+- [Algemene informatie](#algemene-informatie)
+- [Installatie](#installatie)
+- [Deployen](#deployen)
+- [Ontwikkelen](#ontwikkelen)
+- [Bijdragen](#bijdragen)
+- [Licentie](#licentie)
+
+## Algemene informatie
+Deze package wordt gebruikt binnen het curriculumproject om alle leerroutes van de opleiding HBO-ICT van Windesheim visueel weer te geven op een gemakkelijke manier voor studenten.
+
+Om Leerroutes op zo veel mogelijke plaatsen beschikbaar te maken zal het ontwikkeld worden met Javascript webcomponent en dus geen framework bevatten. 
+
+### Functies
+Het zal op termijn mogelijk zijn om de leerroutes te bekijken met behulp van metrolijnen zoals in deze afbeelding met:
+
+(afbeelding hier)
+
+Hiervoor zal [d3-force](https://d3js.org/d3-force#d3-force) gebruikt worden van [d3](https://d3js.org/) om dit te realiseren.
+
+## Installatie
+Deze npm-package kan worden geïnstalleerd via (zodra mogelijk):
+
+```bash
+npm install leerroutes
+```
+of
+```bash
+yarn add leerroutes
+```
+
+## Deployen
+Zodra de package gereed is om te deployen, zal er een workflow worden uitgevoerd wanneer er iets wordt gepusht naar main. Deze workflow zal de code builden en dan pushen naar npm
+
+## Ontwikkelen
+Als je wilt bijdragen aan de Leerroutes-package, zijn er een paar belangrijke dingen om te weten. \
+Allereerst wordt er gebruik gemaakt van [Yarn](https://yarnpkg.com/) en Yarn  Workspaces om de package in `./src/leerroutes` af te scheiden van de testsite in `./src/test-site`.
+
+Om te gaan ontwikkelen voor Leerroutes zijn zal je deze commando's moeten uitvoeren:
+
+Ten eerste moet leerroutes gebuild worden door middel van [Webpack](https://webpack.js.org/):
+```bash
+yarn workspace leerroutes build
+```
+en dan kan je het direct gebruiken met `test-site` via:
+```bash
+yarn workspace test-site start
+``` 
+
+>Merk op dat de leerroutes-npm-package niet direct beschikbaar is in de testsite, je kunt deze toevoegen door het volgende uit te voeren:
+```bash
+yarn workspace test-site link `leerroutes`
+```
+
+### Bijdragen
+Om bij te dragen aan dit project kun je een fork maken en vervolgens pull-requests maken naar deze repository.
+
+Voor informatie, problemen en ideeën wordt verwezen naar het volgende:
+- [Project](https://github.com/orgs/Windesheim-HBO-ICT/projects/4)
+- [Issues](https://github.com/Windesheim-HBO-ICT/Leerroutes/issues)
+- [Discussions](https://github.com/Windesheim-HBO-ICT/Leerroutes/discussions)
+
+Zorg er ook voor dat je comments achterlaat in je code als documentatie voor toekomstige studenten en ontwikkelaars die bijdragen aan het project.
+
+## Licentie
+Dit project heeft momenteel nog geen licentie.
--- src/leerroutes/src/components/LeerrouteWorkspace.js
+++ src/leerroutes/src/components/LeerrouteWorkspace.js
@@ -1,5 +1,7 @@
 import * as d3 from 'd3';
 
+// https://d3js.org/d3-force#d3-force
+// https://observablehq.com/@d3/force-directed-graph/2?intent=fork
 export class LeerrouteWorkspace extends HTMLElement {
     constructor() {
         super();
@@ -11,7 +13,7 @@ export class LeerrouteWorkspace extends HTMLElement {
         const height = 600;
         const color = d3.scaleOrdinal(d3.schemeCategory10);
 
-        // Sample data (replace this with your actual data)
+        // Sample data, in the future this should get set by test-site
         const data = {
             nodes: [
                 { id: 'A', group: 1 },
@@ -28,12 +30,14 @@ export class LeerrouteWorkspace extends HTMLElement {
         const links = data.links.map(d => ({ ...d }));
         const nodes = data.nodes.map(d => ({ ...d }));
 
+        // Simulation is essentially the workspace where all the nodes and links will appear
         const simulation = d3.forceSimulation(nodes)
             .force("link", d3.forceLink(links).id(d => d.id))
             .force("charge", d3.forceManyBody())
             .force("center", d3.forceCenter(width / 2, height / 2))
             .on("tick", ticked);
 
+        // svg is used to display the nodes and links
         const svg = d3.select(this.shadowRoot)
             .append("svg")
             .attr("width", width)
@@ -41,6 +45,7 @@ export class LeerrouteWorkspace extends HTMLElement {
             .attr("viewBox", [0, 0, width, height])
             .attr("style", "max-width: 100%; height: auto;");
 
+        // Links between nodes, in the future this should be done dynamically to create metro lines
         const link = svg.append("g")
             .attr("stroke", "#999")
             .attr("stroke-opacity", 0.6)
@@ -49,6 +54,7 @@ export class LeerrouteWorkspace extends HTMLElement {
             .join("line")
             .attr("stroke-width", d => Math.sqrt(d.value));
 
+        // Node, in the future this should show the semesters like OOSDD
         const node = svg.append("g")
             .attr("stroke", "#fff")
             .attr("stroke-width", 1.5)
@@ -61,11 +67,13 @@ export class LeerrouteWorkspace extends HTMLElement {
         node.append("title")
             .text(d => d.id);
 
+        //Drag them, to be removed in the future
         node.call(d3.drag()
             .on("start", dragstarted)
             .on("drag", dragged)
             .on("end", dragended));
 
+        //A tick from the simulation, seems like you can change things in a tick akin to a useEffect() but for every simulation tick
         function ticked() {
             link
                 .attr("x1", d => d.source.x)
@@ -78,6 +86,7 @@ export class LeerrouteWorkspace extends HTMLElement {
                 .attr("cy", d => d.y);
         }
 
+        // Drag events, to be removed
         function dragstarted(event) {
             if (!event.active) simulation.alphaTarget(0.3).restart();
             event.subject.fx = event.subject.x;
@@ -94,6 +103,7 @@ export class LeerrouteWorkspace extends HTMLElement {
             event.subject.fx = null;
             event.subject.fy = null;
         }
+        // Drag events
 
         simulation.on("end", () => {
             simulation.stop();
